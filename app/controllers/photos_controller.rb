54415a75b0d1264ede58ec2a6c92b5dcb5b69c4d
garzarodrigo <rgarza2016@kellogg.northwestern.edu>
2015-05-11 00:33:00 -0500

    wk_create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,20 @@ class PhotosController < ApplicationController
     @individualphoto = Photo.find(params["id"])
   end
 
+  def new_form
+
+  end
+
+  def create_row
+
+    t = Photo.new
+    t.source = params["the_source"]
+    t.caption = params["the_caption"]
+    t.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+
+
 end
