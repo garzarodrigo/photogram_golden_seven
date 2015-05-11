d16dd100a6f207e2cf4a036f0505dd7535799236
garzarodrigo <rgarza2016@kellogg.northwestern.edu>
2015-05-11 01:38:35 -0500

    wk_finished

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -21,6 +21,27 @@ class PhotosController < ApplicationController
     redirect_to("http://localhost:3000/photos")
   end
 
+  def destroy
+
+    t = Photo.find(params["id"])
+    t.destroy
+
+  end
+
+  def edit_form
+    @photoupdate = Photo.find(params["id"])
+  end
+
+  def update_row
+
+    t = Photo.find(params["id"])
+    t.source = params["the_source"]
+    t.caption = params["the_caption"]
+    t.save
+
+    redirect_to("http://localhost:3000/photos/#{t.id}")
+
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get("/photos/new", { :controller => "photos", :action => "new_form" })
   get("/create_photo", { :controller => "photos", :action => "create_row" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
-  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" }
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+  get("/update_photo/:id", { :controller => "photos", :action => "update_row" })
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
 
 end
